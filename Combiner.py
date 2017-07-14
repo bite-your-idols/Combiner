e585ce31b3bd9979b21ca636cf6706d726ae4dad
Josu <josu@onclick.es>
2017-07-14 13:54:20 +0200

    multiple combiner declarations in same file

--- Combiner.py
+++ Combiner.py
@@ -5,13 +5,9 @@ class CombinerCommand(sublime_plugin.TextCommand):
         # ---
         # definimos variables iniciales
         # ---
-        archive_list=""
-        pattern = '/{0,2}@combine_css\( *\'(.+)\' *\)' 
-        pattern_output= '/{0,2}@output_css\( *\'(.+)\' *\)' 
-
         full_name = self.view.file_name()
         extension = full_name.split('.')[-1]
-        # file_name = '.'.join(full_name.split('.')[0:-1])
+        file_name = '.'.join(full_name.split('.')[0:-1])
         file=full_name.split('\\')[-1]
         path=full_name.split(file)[0]
 
@@ -25,47 +21,128 @@ class CombinerCommand(sublime_plugin.TextCommand):
         # print (allcontent)
 
         # ---
-        # cogemos el archivo de destino
+        # Los patters de extraer los strings
         # ---
-        try:
-            archive_output = re.search(pattern_output,allcontent).group(1)
-            archive_output = path + archive_output
-        except AttributeError:
-            archive_output = path + '.comb.' + extension
-
-        print(archive_output)
-
+        pattern_block = 'Combiner.*?\}'
+        pattern_file = '/{0,2}combine:*\"(.+)\" *,' 
+        pattern_output= '/{0,2}output:*\"(.+)\" *'
 
         # ---
-        # loop cogiendo los elementos a concatenar
+        # Bucle cogiendo cada bloque de combiner presente en el archivo
         # ---
-        for m in re.finditer(pattern, allcontent):
-            # print('found:', m.start(), m.end())
-            temp_name =sublime.Region(m.start(), m.end())
-            temp_name = self.view.substr(temp_name)
-            temp_name= m.group(1)
-            # print(temp_name)
-            archive_list=archive_list+" "+path+temp_name
+        for temp_block in re.findall(pattern_block,allcontent,re.DOTALL):
+
+            archive_list=""
+            # print("---->")
+            # print(temp_block)
+
+            #---
+            # loop cogiendo los elementos a concatenar
+            # ---
+            for m in re.finditer(pattern_file, temp_block):
+                # print('found:', m.start(), m.end())
+                temp_name =sublime.Region(m.start(), m.end())
+                temp_name = self.view.substr(temp_name)
+                temp_name= m.group(1)
+                # print(temp_name)
+                archive_list=archive_list+" "+path+temp_name
+
+            #---
+            # cogemos el archivo de destino
+            #---
+            try:
+                archive_output = re.search(pattern_output,temp_block).group(1)
+                archive_output = path + archive_output
+            except AttributeError:
+                archive_output = file_name + '.comb.' + extension
+
+            # print(archive_output)
+
+            # ---
+            # con todo definido ejecutamos el concatenado
+            # ---
+            arguments=archive_output+archive_list
+            # print ("-> concat -o "+arguments)
+            USE_SHELL = sublime.platform() == 'windows'
+            POPEN_ENV = ({'PATH': ':'.join(['/usr/local/bin', os.environ['PATH']])}) if sublime.platform() == 'osx' and os.path.isdir('/usr/local/bin') else None
+            cmd="concat -o "+arguments
+            subprocess.Popen(cmd, stderr=subprocess.PIPE, shell=USE_SHELL, env=POPEN_ENV)
+
+            # ---
+            # despues abrimos el archivo generado
+            # ---
+            sublime.active_window().open_file(archive_output)
 
 
-        # ---
-        # con todo definido ejecutamos el concatenado
-        # ---
-        # opcion 1: abrir una consola del OS
-        # arguments=archive_output+archive_list
-        # print ("-> concat -o "+arguments)
-        # os.system("concat -o "+arguments)
-        # subprocess.Popen(["concat -o "+arguments])
-
-        # opcion 2: usando subprocess no hace falta abrir una ventana de cmd
-        arguments=archive_output+archive_list
-        print ("-> concat -o "+arguments)
-        USE_SHELL = sublime.platform() == 'windows'
-        POPEN_ENV = ({'PATH': ':'.join(['/usr/local/bin', os.environ['PATH']])}) if sublime.platform() == 'osx' and os.path.isdir('/usr/local/bin') else None
-        cmd="concat -o "+arguments
-        subprocess.Popen(cmd, stderr=subprocess.PIPE, shell=USE_SHELL, env=POPEN_ENV)
 
 
-        # ---
-        # despues habria que abrir el archivo generado
-        # ---
+        # ------------------------------------------------------------------------------------------------------
+        # a partir de aqui metodo de prueba para formato json
+
+
+        # # ---
+        # Los patters de extraer los strings
+        # # ---
+        # pattern_block = '"combine.*?\}'
+        # pattern_files = '/{0,2}files\":\[*(.+) *\]'
+        # pattern_file = '/{0,2}*\"(.+) \"*' 
+        # pattern_output= '/{0,2}output":*\"(.+)\" *'
+
+        # # ---
+        # # Bucle cogiendo cada bloque de combiner presente en el archivo
+        # # ---
+        # for temp_block in re.findall(pattern_block,allcontent,re.DOTALL):
+
+        #     archive_list=""
+        #     print("---->")
+        #     print(temp_block)
+
+        #     #---
+        #     # loop cogiendo los elementos a concatenar
+        #     # ---
+        #     for m in re.finditer(pattern_files, temp_block):
+        #         # print('found:', m.start(), m.end())
+        #         temp_list =sublime.Region(m.start(), m.end())
+        #         temp_list = self.view.substr(temp_list)
+        #         temp_list= m.group(1)
+        #         print("lista:")
+        #         print(temp_list)
+
+
+        #         for m in re.finditer(pattern_file, temp_list):
+        #             # print('found:', m.start(), m.end())
+        #             temp_name =sublime.Region(m.start(), m.end())
+        #             temp_name = self.view.substr(temp_name)
+        #             temp_name= m.group(1)
+        #             print("archivo:")
+        #             print(temp_name)
+        #             print("--")
+        #             archive_list=archive_list+" "+path+temp_name
+
+        #     #---
+        #     # cogemos el archivo de destino
+        #     #---
+        #     try:
+        #         archive_output = re.search(pattern_output,temp_block).group(1)
+        #         archive_output = path + archive_output
+        #     except AttributeError:
+        #         archive_output = file_name + '.comb.' + extension
+
+        #     print(archive_output)
+
+        #     # ---
+        #     # con todo definido ejecutamos el concatenado
+        #     # ---
+        #     arguments=archive_output+archive_list
+        #     print ("-> concat -o "+arguments)
+        #     USE_SHELL = sublime.platform() == 'windows'
+        #     POPEN_ENV = ({'PATH': ':'.join(['/usr/local/bin', os.environ['PATH']])}) if sublime.platform() == 'osx' and os.path.isdir('/usr/local/bin') else None
+        #     cmd="concat -o "+arguments
+        #     subprocess.Popen(cmd, stderr=subprocess.PIPE, shell=USE_SHELL, env=POPEN_ENV)
+
+        #     # ---
+        #     # despues abrimos el archivo generado
+        #     # ---
+        #     # sublime.active_window().open_file(archive_output)
+
+       
